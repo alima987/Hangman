d4df1a153210f8170f35077b0b8d8245a9a18da4
alima987 <alima1996b@gmail.com>
2024-05-28 17:11:00 +0500

    feat: add html structure

--- hangman/main.js
+++ hangman/main.js
@@ -1,10 +1,34 @@
 import './style.css'
+import gallows from './public/gallows.svg'
 const bodyStructure = () => {
   const html = `
   <header class="header">
    <h2>Hangman Game</h2>
-  </hedaer>
-  <div>
+  </header>
+  <div class="container">
+<div class="gallow">
+<img src="${gallows}" alt="gallow">
+</div>
+<div class="quiz">
+<div class="words">
+<ul>
+<li class='letter'></li>
+<li class='letter'></li>
+<li class='letter'></li>
+<li class='letter'></li>
+<li class='letter'></li>
+<li class='letter'></li>
+</ul>
+</div>
+<div class="hint">
+<p>Hint:</p>
+</div>
+<div class="incorrect">
+<p>Incorrect guesses:</p>
+</div>
+<div class="virtual_keyboard">
+</div>
+</div>
   </div>
   `;
   const root = document.createElement('div')
